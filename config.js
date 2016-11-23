f7efdd7692d3d89f4defce0cd7571bfe2b72fe5f
richardsonian <ianrichardson@Ians-MacBook-Pro.local>
2016-11-23 16:34:10 -0500

    Add JS
    On submit button press, parses data and sends back to watch.

---
+++ config.js
@@ -0,0 +1,54 @@
+var dom = {
+  'dayConfigCheckboxes': document.querySelectorAll('input[type=checkbox]'),
+  'submitButton': document.getElementById("submit_button"),
+  'defaultClasses': document.querySelectorAll('input.defaultClass_input'),
+  'dayConfigClasses': document.querySelectorAll('input.dayConfigClass_input')
+};
+
+dom.submitButton.addEventListener('click', function() {
+  var classString[8];
+
+  for(var i=0; i<8; i++) {
+    if(!dom.dayConfigCheckboxes[i].checked) {
+      classString[i] = defaultClasses[i].value.concat('|'.concat(defaultClasses[i].value).repeat(7));
+      console.log(classString[i]);
+    }
+    else {
+      classString[i]=dayConfigClasses[i*8];
+      for(var k=1; k<8; k++) {
+        classString[i].concat('|');
+        classString[i].concat(classString[i].concat(dayConfigClasses[(i*8)+k]));
+      }
+    }
+    console.log(classString[i]);
+  }
+
+  var options = {
+    'ABlockClasses': classString[0],
+    'BBlockClasses': classString[1],
+    'CBlockClasses': classString[2],
+    'DBlockClasses': classString[3],
+    'EBlockClasses': classString[4],
+    'FBlockClasses': classString[5],
+    'GBlockClasses': classString[6],
+    'HBlockClasses': classString[7],
+  };
+
+
+  // Determine the correct return URL (emulator vs real watch)
+  function getQueryParam(variable, defaultValue) {
+    var query = location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1]);
+      }
+    }
+    return defaultValue || false;
+  }
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+
+  // Encode and send the data when the page closes
+  document.location = return_to + encodeURIComponent(JSON.stringify(options));
+});
